3a544351637583bbcc8543042cb809751a6ab09a
Maenda Fortier <maenda@pixelunion.net>
2019-03-17 13:36:22 -0400

    Updates

---
+++ precache-manifest.e4257523789701ed462be9a87052fcbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.1fdedca4.js"
+  },
+  {
+    "revision": "de7ec4ad8565a1a286d0",
+    "url": "/tic-tac-toe/static/js/main.4bd8051d.chunk.js"
+  },
+  {
+    "revision": "036f1fd0e44e3723d5f1",
+    "url": "/tic-tac-toe/static/js/2.ecf5305d.chunk.js"
+  },
+  {
+    "revision": "de7ec4ad8565a1a286d0",
+    "url": "/tic-tac-toe/static/css/main.8589ed1e.chunk.css"
+  },
+  {
+    "revision": "e73e3a8b2d859d5b5239b92d2320ee81",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
